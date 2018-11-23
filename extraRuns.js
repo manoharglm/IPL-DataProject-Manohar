4db8c53057df39bc41857a3458727e580657440e
manoharglm <manohargunduboina@gmail.com>
2018-11-23 12:39:19 +0530

    The extra runs conceded per team-Fixed incorrect ouput.

--- extraRuns.js
+++ extraRuns.js
@@ -5,14 +5,13 @@ let deliveries=DataBase.split("\n");
 let extras={}
 for(let i=1;i<deliveries.length;i++){
 		let match_data=deliveries[i].split(",");
-			if((match_data[0]>=match_id[0]) && match_data[0]<=match_id[match_id.length-1]){
+			if((parseInt(match_data[0])>=match_id[0]) && parseInt(match_data[0])<=match_id[match_id.length-1]){
 				if(extras[match_data[2]]){
-					extras[match_data[2]]+=Number(match_data[16]);
+					extras[match_data[2]]=Number(extras[match_data[2]])+Number(match_data[16]);
 				}
 			else{
 				extras[match_data[2]]=Number(match_data[16]);
 			}
 		}
 }
-console.log(match_id[match_id.length-1])
 console.log(extras);
